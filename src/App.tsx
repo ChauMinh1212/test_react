d2efdba769ec6d1f2753d263e60adad6204ee475
Chau Minh <minhnlc@aloapp.vn>
2024-06-21 15:10:28 +0700

    them excel

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import AuthPage from './pages/AuthPage/AuthPage'
 import HomePage from './pages/HomePage/HomePage'
+import ListStockPage from './pages/ListStockPage/ListStockPage'
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/auth' element={<AuthPage />} />
+        <Route path='/list' element={<ListStockPage />} />
         {/* <Route path='/:username' element={<ProfilePage />} /> */}
       </Routes>
       {/* <Footer></Footer> */}
